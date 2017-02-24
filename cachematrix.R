abc51b073a8de7bbc11b2fad0047c4adf5baec1d
Len Greski <leonard.m@greski.com>
2017-02-24 06:59:43 -0500

    Add logic to test whether the input object is of type matrix()

--- cachematrix.R
+++ cachematrix.R
@@ -49,8 +49,8 @@ cacheSolve <- function(x, ...) {
      
      ## invert the matrix, set the cache, and return
      data <- x$get()
-     if (is.na(data) == TRUE) {
-          stop("object passed to cacheSolve() is empty, cannot calculate its inverse.")
+     if (!is.matrix(data)) {
+          stop("object passed to cacheSolve() is not a valid matrix, cannot calculate its inverse.")
      }
      ## check to see whether matrix is invertible, meaning that
      ## the determinant must be non-zero
