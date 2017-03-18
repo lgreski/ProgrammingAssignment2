a3facaf9d07565c27750b41ddc77895c759fc861
Leonard Greski <len@ageretechnology.com>
2017-03-18 12:30:30 -0400

    Add matrix multiplication to demonstrate that A * A(inv) = Identity matrix

--- testCachematrix.R
+++ testCachematrix.R
@@ -3,13 +3,11 @@
 a <- makeCacheMatrix(matrix(c(-1, -2, 1, 1), 2,2))
 cacheSolve(a)
 
-# approach 2: use makeCacheMatrix() as the input argument to cacheSolve()
-#             note that the argument to cacheSolve() is a different object
-#             than the argument to the first call of cacheSolve()
-cacheSolve(makeCacheMatrix(matrix(c(-1, -2, 1, 1), 2,2)))
-
 # call cacheSolve(a) a second time to trigger the "getting cached inverse" message
 cacheSolve(a)
+
+# multiply the matrix by inverse, resulting in identity matrix
+a$get() %*% a$getsolve()
 
 # reset a with another matrix to clear out cached value
 a$set(matrix(c(2,3,2,2),2,2))
@@ -21,6 +19,11 @@ a$getsolve()
 # rerun cache solve, note that "getting cached inverse" does not print,
 # and that we get a different result
 cacheSolve(a)
+
+# approach 2: use makeCacheMatrix() as the input argument to cacheSolve()
+#             note that the argument to cacheSolve() is a different object
+#             than the argument to the first call of cacheSolve()
+cacheSolve(makeCacheMatrix(matrix(c(-1, -2, 1, 1), 2,2)))
 
 # try a non-invertible matrix
 b <- makeCacheMatrix(matrix(c(0,0,0,0),2,2))
